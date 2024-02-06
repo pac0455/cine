65b6168c0c5297392eec4b78db0349b2138dfe19
pac0455 <franhidalc@gmail.com>
2024-02-06 18:07:11 +0100

    terminar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,6 +33,25 @@ const Home = () => {
                 </Slider>
             </section>
             <FontAwesomeIcon className="animate-bounce m-5" size="4x" icon={faAnglesDown} />
+            <div className="p-4 w-[80%] shadow-2xl shadow-black h-96 m-auto mt-6 rounded-xl mb-6 flex">
+                <div className="w-1/2 bg-white/80 h-96 rounded-xl p-3 ">
+                    <h1 className="text-black font-medium text-5xl ">Ventajas: </h1>
+                    <ul className="text-black font-medium text-left mt-5">
+                        <li className="text-black font-medium text-left mt-5">Barato</li>
+                        <li className="text-black font-medium text-left mt-5">Comodidad en nuestra salas de sitios</li>
+                        <li className="text-black font-medium text-left mt-5">Buena ubicación</li>
+                        <li className="text-black font-medium text-left mt-5">Rapidez a la hora de pedir entradas</li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div className="w-1/2 rounded-xl p-4">
+                <h1 className="text-white font-medium text-5xl ">Desventajas: </h1>
+                    <ul className="text-white font-medium text-left mt-5">
+                        <li className="text-white font-medium text-left mt-5">Sin tiempo para ver todas las peliculas</li>
+                        <li className="text-white font-medium text-left mt-5">No poder venir a nuestro cine todos los dias</li>
+                    </ul>
+                </div>
+            </div>
             <Ofertas img={data[18].poster_path}/>
         </>
     );
--- src/pages/reservar.jsx
+++ src/pages/reservar.jsx
@@ -1,38 +1,36 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChair } from '@fortawesome/free-solid-svg-icons';
+import palomitas from '../assets/palomitas.png';
 
 const Reservar = () => {
-    let pillada = true
-    const handleReserva = () => {
-        console.log(pillada);
-        if (pillada) {
-            pillada = false
-        } else {
-            pillada = true
-        }
-    }
-    const array = [];
-    for (let i = 0; i < 40; i++) {
-        array.push(i);
-    }
 
     return (
         <>
-            <div className='h-96  grid grid-cols-12 border border-1 border-solid border-black p-3'>
-                {array.map((e, index) => (
-                    pillada ? (
-                        <div className='w-12 h-12 relative bg-green-600 rounded-lg' key={index}> {/* Cambiado el tamaño del contenedor */}
-                            <FontAwesomeIcon className="absolute top-4 left-3" size="2x" icon={faChair} /> {/* Cambiado el tamaño del icono */}
-                        </div>
-                    ) : (
-                        <div className='w-12 h-12 relative bg-red-600 rounded-lg' key={index}> {/* Cambiado el tamaño del contenedor */}
-                            <FontAwesomeIcon className="absolute top-4 left-3" size="2x" icon={faChair} /> {/* Cambiado el tamaño del icono */}
-                        </div>
-                    )
-                ))}
+            <div class="bg-gray-900 h-screen flex flex-col items-center justify-center text-center">
+                <img src={palomitas} alt="" srcset="" />
+                <div class="mt-8">
+                    <form action="https://fabform.io/f/xxxxx" method="post" class="flex flex-col items-center">
+                        <input type="text" name="name" placeholder="Your Full Name" class="py-2 px-4 bg-gray-800 text-white rounded-md focus:outline-none mb-4" required />
+                        <input type="email" name="email" placeholder="Your Email Address" class="py-2 px-4 bg-gray-800 text-white rounded-md focus:outline-none mb-4" required />
+                        <label className='text-3xl ' htmlFor="Fila">Fila: </label>
+                        <select className='w-64 bg-[#1f2937] p-2 text-lg font-medium outline-none  mb-4' name="" id="">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </select>
+                        <input type="email" name="email" placeholder="Tu pelicula" class="py-2 px-4 bg-gray-800 text-white rounded-md focus:outline-none mb-4" required />
+                        <textarea name="message" placeholder="Your Message" rows="4" class="py-2 px-4 bg-gray-800 text-white rounded-md focus:outline-none mb-4 resize-none w-full" required></textarea>
+                        <button type="submit" class="bg-blue-500 py-2 px-4 text-white rounded-md hover:bg-blue-600 focus:outline-none">Send</button>
+                       
+                    </form>
+                    <p class="mt-2 text-gray-400 text-sm">We'll get back to you as soon as possible!</p>
+                    <div class="mt-4 text-gray-500 text-sm">
+                        Powered by <a href="https://fabform.io" class="underline" target="_blank" rel="noopener noreferrer">FabForm.io</a>
+                    </div>
+                </div>
             </div>
-            <button className="w-64 text-3xl p-3 bg-[#961e11] mt-7">Reservar</button>
         </>
     );
 };
